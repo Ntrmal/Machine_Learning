c2417a71ae1d621b0146942e5f322aa17c3ce9f4
Ntrmal <122348344+Ntrmal@users.noreply.github.com>
2023-12-12 18:55:05 +0100

    Add files via upload

---
+++ AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿
+using MongoDB.Driver;
+using static VoltagePrediction.MLModel;
+namespace VoltagePrediction
+{
+
+
+    public class AppDbContext
+    {
+        private readonly IMongoDatabase _database;
+
+        public AppDbContext()
+        {
+            var client = new MongoClient("mongodb://localhost:27017");
+            _database = client.GetDatabase("simple_db");
+        }
+
+        public IMongoCollection<Trucksinfo> TruckData => _database.GetCollection<Trucksinfo>("TruckData");
+    }
+}
---
+++ Truck.cs
@@ -0,0 +1,29 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json.Serialization;
+namespace VoltagePrediction
+{
+
+    public class Trucksinfo
+    {
+
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string _id { get; set; }
+
+
+        public string sku { get; set; }
+        public int tenantId { get; set; }
+
+        public string identifier { get; set; }
+        public string serialNumber { get; set; }  
+
+        [BsonRepresentation(BsonType.String)]
+        public DateTimeOffset originTime { get; set; }     
+
+        public BsonDocument payload { get; set; }
+    }
+}
+
+
